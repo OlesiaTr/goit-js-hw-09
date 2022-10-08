7e6d13d9602b37b99cda09486047f9596c6be613
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2022-10-08 17:54:36 +0000

    Deploying to gh-pages from @ OlesiaTr/goit-js-hw-09@453ff498d243d56670091b57329de1618107c075 🚀

---
+++ 01-color-switcher.62cd2df8.js
@@ -0,0 +1,2 @@
+const t={start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]"),body:document.querySelector("body")};let e=null;t.start.addEventListener("click",(function(o){o.target.disabled=!0,o.target.disabled&&(e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3))})),t.stop.addEventListener("click",(function(){t.start.disabled=!1,clearInterval(e)}));
+//# sourceMappingURL=01-color-switcher.62cd2df8.js.map
