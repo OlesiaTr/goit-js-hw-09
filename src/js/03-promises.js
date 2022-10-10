db477644321810a4c0bcda6eedd44af541c57cc2
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2022-10-10 21:21:32 +0300

    feat: third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  form: document.querySelector('.form'),
+  delay: document.querySelector('[name="delay"]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+};
+
+refs.form.addEventListener('submit', onSubmit);
+
+// Chooses whether to fulfill or reject the promise.
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  // Gets values from inputs.
+  let delay = Number(refs.delay.value);
+  const step = Number(refs.step.value);
+  const amount = Number(refs.amount.value);
+
+  for (let position = 1; position <= amount; position += 1) {
+    // Returns one promise that will be fulfilled or rejected after delay time.
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        return Notify.success(`Fullfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        return Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+    // Adds delay on next step after each promise.
+    delay += step;
   }
 }
